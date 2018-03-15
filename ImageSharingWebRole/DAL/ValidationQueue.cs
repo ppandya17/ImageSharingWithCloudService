a83599eb559351e5058487bb95473ecccdbc030e
Parth Pandya <pspandya2009@gmail.com>
2018-03-15 02:07:44 -0400

    Update entire project on Github

---
+++ ImageSharingWebRole/DAL/ValidationQueue.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System.Threading;
+using System.Net;
+using Microsoft.Azure;
+using ImageSharingWebRole.Models;
+
+namespace ImageSharingWebRole.DAL
+{
+    public class ValidationQueue
+    {
+        
+        public const string QueueName = "ValidationQueue";
+        static QueueClient Client;
+
+        public static void Initialize()
+        {
+            try
+            {
+                // Set the maximum number of concurrent connections 
+                ServicePointManager.DefaultConnectionLimit = 12;
+
+                // Create the queue if it does not exist already
+                string connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+                var namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+                if (!namespaceManager.QueueExists(QueueName))
+                {
+                    namespaceManager.CreateQueue(QueueName);
+                }
+                Client = QueueClient.CreateFromConnectionString(connectionString, QueueName);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static void Send(ValidationRequest Request)
+        {
+            if (Client == null)
+            {
+                Initialize();
+            }
+
+            try
+            {   
+                Client.Send(new BrokeredMessage(Request));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Finalize()
+        {
+            // Close the connection to Service Bus Queue
+            Client.Close();
+        }
+    }
+}
